df4839c2f5f0f7476244d11293dd0a5c78bf1d57
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2019-03-10 03:24:04 +0200

    Initial build

---
+++ precache-manifest.e2b238ed147fd558a4d0ac47bf35325b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bcf0e1999ccf1fed4e8",
+    "url": "/calendar/static/js/runtime~main.8bcf0e19.js"
+  },
+  {
+    "revision": "eb203e972d9dc2db068a",
+    "url": "/calendar/static/js/main.eb203e97.chunk.js"
+  },
+  {
+    "revision": "c8a7815e0ae2b3662e9c",
+    "url": "/calendar/static/js/2.c8a7815e.chunk.js"
+  },
+  {
+    "revision": "eb203e972d9dc2db068a",
+    "url": "/calendar/static/css/main.2d0e630a.chunk.css"
+  },
+  {
+    "revision": "319b10782288aaec71fa0df58710a6e0",
+    "url": "/calendar/index.html"
+  }
+];
