29eec361b8e42ab5d6dfe8f36fa47e95b144df9c
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2019-03-10 15:11:59 +0200

    Updates

---
+++ precache-manifest.31fbc37988658a3fb47dd4bac1b75fe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bcf0e1999ccf1fed4e8",
+    "url": "/calendar/static/js/runtime~main.8bcf0e19.js"
+  },
+  {
+    "revision": "34a787f22dad2bb6b169",
+    "url": "/calendar/static/js/main.34a787f2.chunk.js"
+  },
+  {
+    "revision": "b98bb320dcc6dc1e91c4",
+    "url": "/calendar/static/js/2.b98bb320.chunk.js"
+  },
+  {
+    "revision": "34a787f22dad2bb6b169",
+    "url": "/calendar/static/css/main.772d8f84.chunk.css"
+  },
+  {
+    "revision": "330c896a2855391b22ca6408821c6d4b",
+    "url": "/calendar/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/calendar/precache-manifest.c46957f3ef8b8e61c7738782ef94b021.js"
+  "/calendar/precache-manifest.31fbc37988658a3fb47dd4bac1b75fe1.js"
 );
 
 workbox.clientsClaim();
