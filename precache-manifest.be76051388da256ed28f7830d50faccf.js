c0955e72ce568b1f25f4d6eb36e3925cc41241bc
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2019-03-11 14:27:22 +0200

    Updates

---
+++ precache-manifest.be76051388da256ed28f7830d50faccf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5ae921c4e37a5c5535e",
+    "url": "/react-week-scheduler/static/js/runtime~main.d5ae921c.js"
+  },
+  {
+    "revision": "3983c602e042f5f46440",
+    "url": "/react-week-scheduler/static/js/main.3983c602.chunk.js"
+  },
+  {
+    "revision": "b006c96325558cb5505d",
+    "url": "/react-week-scheduler/static/js/2.b006c963.chunk.js"
+  },
+  {
+    "revision": "3983c602e042f5f46440",
+    "url": "/react-week-scheduler/static/css/main.f8e23ba2.chunk.css"
+  },
+  {
+    "revision": "6c8071c749498d4221d09604e48e37ef",
+    "url": "/react-week-scheduler/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/calendar/precache-manifest.4e79056270c96c272d5b82023cb25ad6.js"
+  "/react-week-scheduler/precache-manifest.be76051388da256ed28f7830d50faccf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/calendar/index.html", {
+workbox.routing.registerNavigationRoute("/react-week-scheduler/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
